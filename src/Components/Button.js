ff655dc1cb8bd2cf4e415cfe4318d8a46a0326a5
Ali Chaghou <87231705+Ali-Chaghou@users.noreply.github.com>
2023-05-14 20:52:57 +0200

    first commit

---
+++ src/Components/Button.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Button() {
+  return (
+    <div>
+      <Link to="/https://id.atlassian.com/login">Jetzt IT-Ticket erstellen</Link>
+    </div>
+  );
+}
+
+export default Button;
